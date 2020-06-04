4789263879bf2d1bf8940295bbead871de74c0db
Jakub Janik <jakubjanik@vp.pl>
2020-06-04 11:31:47 +0200

    Update form styles and validation

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-document.querySelector('form').addEventListener('submit', event => {
+document.querySelector('form').addEventListener('submit', event => {    
     event.preventDefault()
-    const inputs = document.querySelectorAll('input')
 
-    inputs.forEach(input => {
-        if (input.placeholder === 'Email Address' && !validateEmail(input.value)) {
-            input.nextElementSibling.innerText = 'Looks like this is not an email'
-            input.classList.add('error')
-        } 
-
-        if (! input.value.length) {
-            input.nextElementSibling.innerText = `${input.placeholder} cannot be empty`
-            input.classList.add('error')
+    for (const input of event.target) {
+        if (input.validity.valueMissing) {
+            input.parentElement.classList.add('error')
+            input.nextElementSibling.textContent = `${input.placeholder} cannot be empty`
         }
-    })
-})
 
-const validateEmail = email => /^\S+@\S+$/.test(email)
+        if (input.placeholder == 'Email Address' && input.validity.typeMismatch) {
+            input.parentElement.classList.add('error')
+            input.nextElementSibling.textContent = 'Looks like this is not an email'
+        }
+    }
+})
