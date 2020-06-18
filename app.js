789f60d3991c771b1f6a389028f8a10d499557cd
Jakub Janik <jakubjanik@vp.pl>
2020-06-18 11:46:13 +0200

    Make some corrections

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
-document.querySelector('form').addEventListener('submit', event => {    
+const form = document.querySelector('form')
+form.addEventListener('submit', validateForm)
+
+function validateForm(event) {
     event.preventDefault()
 
-    for (const input of event.target) {
+    for (const input of [...this].slice(0, -1)) {
+        const control = input.closest('.control')
+        const error = control.querySelector('.error-message')
+
         if (input.validity.valueMissing) {
-            input.parentElement.classList.add('error')
-            input.nextElementSibling.textContent = `${input.placeholder} cannot be empty`
+            control.classList.add('error')
+            error.textContent = `${input.placeholder} cannot be empty`
         }
 
         if (input.placeholder == 'Email Address' && input.validity.typeMismatch) {
-            input.parentElement.classList.add('error')
-            input.nextElementSibling.textContent = 'Looks like this is not an email'
+            control.classList.add('error')
+            error.textContent = 'Looks like this is not an email'
         }
 
         if (input.checkValidity()) {
-            input.parentElement.classList.remove('error')
+            control.classList.remove('error')
         }
     }
-})
+}
